2bd2ce2fcc157eaa323b2063903685a7f172c6d9
Kien Vu <kvstudio.154@gmail.com>
2021-10-22 16:25:44 +0700

    22/10/2021 4:25 PM
    Daily backup

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pakingmanagementsystem {
-  requires javafx.controls;
-  requires javafx.fxml;
-  requires javafx.graphics;
-
-
-  opens com.example.pakingmanagementsystem to javafx.fxml;
-  exports com.example.pakingmanagementsystem;
-}

811f2f4fbb0cbb7cc9b3e73a11a6b0ea9ed89dd3
Kien Vu <kvstudio.154@gmail.com>
2021-10-22 17:48:09 +0700

    22/10/2021 5:48 PM
    Daily backup

--- src/main/java/com/example/pakingmanagementsystem/HelloApplication.java
+++ src/main/java/com/example/pakingmanagementsystem/HelloApplication.java
@@ -1,11 +1,11 @@
 package com.example.pakingmanagementsystem;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
   public static void main(String[] args) {
@@ -14,6 +14,11 @@ public class HelloApplication extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-
+    //Stage stage = new Stage();
+    Group root = new Group();
+    Scene scene = new Scene(root, Color.rgb(200,60,185));
+    stage.setTitle("Parking Management System");
+    stage.setScene(scene);
+    stage.show();
   }
 }

51cb2ce9ece22150e9375498406352089c51158c
Kien Vu <kvstudio.154@gmail.com>
2021-10-22 19:58:29 +0700

    22/10/2021 7:58 PM
    Daily backup

--- src/main/java/com/example/pakingmanagementsystem/HelloApplication.java
+++ src/main/java/com/example/pakingmanagementsystem/HelloApplication.java
@@ -3,9 +3,9 @@ package com.example.pakingmanagementsystem;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 
 public class HelloApplication extends Application {
   public static void main(String[] args) {
@@ -16,7 +16,9 @@ public class HelloApplication extends Application {
   public void start(Stage stage) throws Exception {
     //Stage stage = new Stage();
     Group root = new Group();
-    Scene scene = new Scene(root, Color.rgb(200,60,185));
+    Scene scene = new Scene(root, Color.WHITESMOKE);
+    Image icon = new Image("images/icon.jpg");
+    stage.getIcons().add(icon);
     stage.setTitle("Parking Management System");
     stage.setScene(scene);
     stage.show();

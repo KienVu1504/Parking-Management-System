f550e0fd5a7d14df280e75046a4ef1121765d7b7
Kien Vu <kvstudio.154@gmail.com>
2021-10-31 22:35:58 +0700

    31/10/2021 10:35 PM
    Daily backup

--- src/OutController.java
+++ src/OutController.java
@@ -5,21 +5,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class OutController {
   private Scene scene;
   private Parent root;
   private Stage stage;
   @FXML
-  private Button getOutTimeButton;
+  private Label errorLabel1, errorLabel2, errorLabel;
   @FXML
-  private TextField timeOutField;
+  private Button getOutTimeButton,doneButton;
+  @FXML
+  private TextField ticketTextField, seatTextField, timeOutField, licensePlateTextField, vehicleTypeTextField, timeInTextField, parkingFeeTextField;
   @FXML
   private AnchorPane OutPane;
   public void closeAPP(){
@@ -36,6 +42,25 @@ public class OutController {
       stage.close();
     }
   }
+  public void limitLength() {
+    licensePlateTextField.lengthProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.intValue() > oldValue.intValue()) {
+        // Check if the new character is greater than LIMIT
+        if (licensePlateTextField.getText().length() > 10) {
+          errorLabel.setTextFill(Color.RED);
+          errorLabel.setText("License Plate length must be <= 10!");
+          // if its 11th character then just setText to previous
+          // one
+          licensePlateTextField.setText(licensePlateTextField.getText().substring(0, 10));
+        } else {
+          errorLabel1.setText("");
+        }
+      }
+    });
+  }
+  public void outCheck(){
+
+  }
   @FXML
   private MenuBar menuBar;
   public void goToIn() throws IOException {
@@ -51,7 +76,52 @@ public class OutController {
     timeOutField.setText(dtf.format(now));
   }
   public void licensePlateSearch(){
-
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    try {
+      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/parkingsystem", "root", "");
+      preparedStatement = connection.prepareStatement("select * from parking where license_plate = ?");
+      preparedStatement.setString(1, licensePlateTextField.getText());
+      resultSet = preparedStatement.executeQuery();
+      if (!resultSet.next()) {
+        errorLabel.setTextFill(Color.RED);
+        errorLabel.setText("Can't find " + licensePlateTextField.getText());
+        errorLabel1.setTextFill(Color.RED);
+        errorLabel1.setText("!");
+      } else {
+        errorLabel1.setText("");
+        preparedStatement = connection.prepareStatement("select * from parking where license_plate = ?");
+        preparedStatement.setString(1, licensePlateTextField.getText());
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+          vehicleTypeTextField.setText(resultSet.getString("type"));
+          seatTextField.setText(resultSet.getString("seat"));
+          if (resultSet.getInt("ticket") == 0) {
+            ticketTextField.setText("No");
+          } else {
+            ticketTextField.setText("Yes");
+          }
+          timeInTextField.setText(resultSet.getString("time_in"));
+        }
+      }
+    } catch (SQLException e) {
+      Logger.getLogger(OutController.class.getName()).log(Level.SEVERE, null, e);
+    } finally {
+      try {
+        if (resultSet != null) {
+          resultSet.close();
+        }
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+        if (connection != null) {
+          connection.close();
+        }
+      } catch (SQLException e) {
+        Logger.getLogger(OutController.class.getName()).log(Level.SEVERE, null, e);
+      }
+    }
   }
   public void goToHistory() throws IOException {
     root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HistoryScene.fxml")));

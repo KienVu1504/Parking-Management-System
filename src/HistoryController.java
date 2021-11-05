44842ee16f904621624f3d78e9991866690a35ff
Kien Vu <kvstudio.154@gmail.com>
2021-11-05 16:10:40 +0700

    5/11/2021 4:10 PM
    Daily backup

--- src/HistoryController.java
+++ src/HistoryController.java
@@ -1,7 +1,5 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,10 +14,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import models.History;
+import repositories.HistoryRepository;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.*;
 import java.util.*;
 
 public class HistoryController implements Initializable {
@@ -44,206 +43,61 @@ public class HistoryController implements Initializable {
   }
 
   @FXML
-  private TableView<HistoryController> historyTable;
+  private TableView<History> historyTable;
   @FXML
-  private TableColumn<HistoryController, Integer> IdColumn;
+  private TableColumn<History, Integer> IdColumn;
   @FXML
-  private TableColumn<HistoryController, String> licensePlateColumn;
+  private TableColumn<History, String> licensePlateColumn;
   @FXML
-  private TableColumn<HistoryController, String> vehicleTypeColumn;
+  private TableColumn<History, String> vehicleTypeColumn;
   @FXML
-  private TableColumn<HistoryController, String> seatColumn;
+  private TableColumn<History, String> seatColumn;
   @FXML
-  private TableColumn<HistoryController, Integer> monthlyTicketColumn;
+  private TableColumn<History, Integer> monthlyTicketColumn;
   @FXML
-  private TableColumn<HistoryController, String> timeInColumn;
+  private TableColumn<History, String> timeInColumn;
   @FXML
-  private TableColumn<HistoryController, String> timeOutColumn;
+  private TableColumn<History, String> timeOutColumn;
   @FXML
-  private TableColumn<HistoryController, String> parkingTimeColumn;
+  private TableColumn<History, String> parkingTimeColumn;
   @FXML
-  private TableColumn<HistoryController, String> parkingFeeColumn;
+  private TableColumn<History, String> parkingFeeColumn;
   @FXML
   private TableColumn<HistoryController, Integer> statusColumn;
-  private List<HistoryController> historyControllerList = new ArrayList();
-  private int id, ticket, status;
-  private String license_plate, type, seat, time_in, time_out, parking_time, fee;
+  private List<History> histories = new ArrayList();
+  private HistoryRepository historyRepository = new HistoryRepository();
 
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public void setLicense_plate(String license_plate) {
-    this.license_plate = license_plate;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public void setSeat(String seat) {
-    this.seat = seat;
-  }
-
-  public void setTicket(int ticket) {
-    this.ticket = ticket;
-  }
-
-  public void setTime_in(String time_in) {
-    this.time_in = time_in;
-  }
-
-  public void setTime_out(String time_out) {
-    this.time_out = time_out;
-  }
-
-  public void setParking_time(String parking_time) {
-    this.parking_time = parking_time;
-  }
-
-  public void setFee(String fee) {
-    this.fee = fee;
-  }
-
-  public void setStatus(int status) {
-    this.status = status;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getLicense_plate() {
-    return license_plate;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public String getSeat() {
-    return seat;
-  }
-
-  public int getTicket() {
-    return ticket;
-  }
-
-  public String getTime_in() {
-    return time_in;
-  }
-
-  public String getTime_out() {
-    return time_out;
-  }
-
-  public String getParking_time() {
-    return parking_time;
-  }
-
-  public String getFee() {
-    return fee;
-  }
-
-  public int getStatus() {
-    return status;
-  }
+  //internal state
+  private int pageNumber = 0;
 
   public HistoryController() {
   }
 
-  public HistoryController(int id, String license_plate, String type, String seat, int ticket, String time_in, String time_out, String parking_time, String fee, int status) {
-    this.id = id;
-    this.license_plate = license_plate;
-    this.type = type;
-    this.seat = seat;
-    this.ticket = ticket;
-    this.time_in = time_in;
-    this.time_out = time_out;
-    this.parking_time = parking_time;
-    this.fee = fee;
-    this.status = status;
+  private void pressBack() {
+    pageNumber = pageNumber <= 0 ? pageNumber : pageNumber - 1;
   }
 
+  private void pressNext() {
+    pageNumber++;
+  }
+
+  //select * from parking limit 20 offset 20 * 2
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    Connection connection = null;
-    ResultSet resultSet = null;
-    try {
-      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/parkingsystem", "root", "");
-      PreparedStatement preparedStatement = connection.prepareStatement("select * from parking");
-      resultSet = preparedStatement.executeQuery();
-      while (resultSet.next()) {
-        setId(resultSet.getInt("id"));
-        setLicense_plate(resultSet.getString("license_plate"));
-        setType(resultSet.getString("type"));
-        setSeat(resultSet.getString("seat"));
-        setTicket(resultSet.getInt("ticket"));
-        setTime_in(resultSet.getString("time_in"));
-        setTime_out(resultSet.getString("time_out"));
-        setParking_time(resultSet.getString("parking_time"));
-        setFee(resultSet.getString("fee"));
-        setStatus(resultSet.getInt("status"));
-        historyControllerList.add(new HistoryController(getId(), getLicense_plate(), getType(), getSeat(), getTicket(), getTime_in(), getTime_out(), getParking_time(), getFee(), getStatus()));
-      }
-      ObservableList<HistoryController> historyControllerObservableList = FXCollections.observableArrayList(historyControllerList);
-      IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-      licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("license_plate"));
-      vehicleTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
-      seatColumn.setCellValueFactory(new PropertyValueFactory<>("seat"));
-      monthlyTicketColumn.setCellValueFactory(new PropertyValueFactory<>("ticket"));
-      timeInColumn.setCellValueFactory(new PropertyValueFactory<>("time_in"));
-      timeOutColumn.setCellValueFactory(new PropertyValueFactory<>("time_out"));
-      parkingTimeColumn.setCellValueFactory(new PropertyValueFactory<>("parking_time"));
-      parkingFeeColumn.setCellValueFactory(new PropertyValueFactory<>("fee"));
-      statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-      historyTable.setItems(historyControllerObservableList);
-
-      //Initial filtered list
-      FilteredList<HistoryController> filteredList = new FilteredList<>(historyControllerObservableList, b -> true);
-      searchBox.textProperty().addListener((observable, oldValue, newValue) -> filteredList.setPredicate(historyController -> {
-        //If no search value then display all records or whatever records it current have. No change :v
-        if (newValue.isEmpty() || newValue.isBlank()) {
-          return true;
-        }
-        String searchKeyWords = newValue.toLowerCase();
-        if (historyController.getLicense_plate().toLowerCase().contains(searchKeyWords)) {
-          return true; // Means we found a match in license_plate
-        } else if (historyController.getType().toLowerCase().contains(searchKeyWords)) {
-          return true;
-        } else if (historyController.getSeat().toLowerCase().contains(searchKeyWords)) {
-          return true;
-        } else if (historyController.getTime_in().toLowerCase().contains(searchKeyWords)) {
-          return true;
-        } else if (historyController.getTime_out().toLowerCase().contains(searchKeyWords)) {
-          return true;
-        } else if (historyController.getParking_time().toLowerCase().contains(searchKeyWords)) {
-          return true;
-        } else return historyController.getFee().toLowerCase().contains(searchKeyWords);
-      }));
-      SortedList<HistoryController> sortedList = new SortedList<>(filteredList);
-      // Bind sorted result with TableView
-      sortedList.comparatorProperty().bind(historyTable.comparatorProperty());
-      //Apply filtered and sorted data to the TableView
-      historyTable.setItems(sortedList);
-    } catch (SQLException sqlException) {
-      sqlException.printStackTrace();
-    } finally {
-      if (resultSet != null) {
-        try {
-          resultSet.close();
-        } catch (SQLException sqlException) {
-          sqlException.printStackTrace();
-        }
-      }
-      if (connection != null) {
-        try {
-          connection.close();
-        } catch (SQLException sqlException) {
-          sqlException.printStackTrace();
-        }
-      }
-    }
+    int numberOfItemPerPage = 25;
+    histories = historyRepository.getHistories(pageNumber, numberOfItemPerPage);
+    ObservableList<History> historyControllerObservableList = FXCollections.observableArrayList(histories);
+    IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+    licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("license_plate"));
+    vehicleTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+    seatColumn.setCellValueFactory(new PropertyValueFactory<>("seat"));
+    monthlyTicketColumn.setCellValueFactory(new PropertyValueFactory<>("ticket"));
+    timeInColumn.setCellValueFactory(new PropertyValueFactory<>("time_in"));
+    timeOutColumn.setCellValueFactory(new PropertyValueFactory<>("time_out"));
+    parkingTimeColumn.setCellValueFactory(new PropertyValueFactory<>("parking_time"));
+    parkingFeeColumn.setCellValueFactory(new PropertyValueFactory<>("fee"));
+    statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+    historyTable.setItems(historyControllerObservableList);
   }
 
   @FXML
---
+++ src/repositories/HistoryRepository.java
@@ -0,0 +1,72 @@
+package repositories;
+
+import models.History;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryRepository {
+  public List<History> getHistories(int pageNumber, int numberOfItemPerPage) {
+    List<History> histories = new ArrayList();
+    Connection connection;
+    ResultSet resultSet;
+    try {
+      connection = Database.getInstance().getConnection();
+      String sql = "select * from parking limit ? offset ?";
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setInt(1, numberOfItemPerPage);
+      preparedStatement.setInt(2, pageNumber * numberOfItemPerPage);
+
+      resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        History history = new History();
+        history.setId(resultSet.getInt("id"));
+        history.setLicense_plate(resultSet.getString("license_plate"));
+        history.setType(resultSet.getString("type"));
+        history.setSeat(resultSet.getString("seat"));
+        history.setTicket(resultSet.getInt("ticket"));
+        history.setTime_in(resultSet.getString("time_in"));
+        history.setTime_out(resultSet.getString("time_out"));
+        history.setParking_time(resultSet.getString("parking_time"));
+        history.setFee(resultSet.getString("fee"));
+        history.setStatus(resultSet.getInt("status"));
+        histories.add(history);
+      }
+      return histories;
+    } catch (SQLException sqlException) {
+      System.err.println(String.format("Cannot get histories from DB: ", sqlException.toString()));
+      return histories;
+    }
+  }
+
+  public List<History> getFilteredHistories(String typedText) {
+    List<History> histories = new ArrayList();
+    Connection connection;
+    ResultSet resultSet;
+    try {
+      connection = Database.getInstance().getConnection();
+      PreparedStatement preparedStatement = connection.prepareStatement("select * from parking order where license_plate like %?% ");
+      preparedStatement.setString(1, typedText);
+      resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        History history = new History();
+        history.setId(resultSet.getInt("id"));
+        history.setLicense_plate(resultSet.getString("license_plate"));
+        history.setType(resultSet.getString("type"));
+        history.setSeat(resultSet.getString("seat"));
+        history.setTicket(resultSet.getInt("ticket"));
+        history.setTime_in(resultSet.getString("time_in"));
+        history.setTime_out(resultSet.getString("time_out"));
+        history.setParking_time(resultSet.getString("parking_time"));
+        history.setFee(resultSet.getString("fee"));
+        history.setStatus(resultSet.getInt("status"));
+        histories.add(history);
+      }
+      return histories;
+    } catch (SQLException sqlException) {
+      System.err.println(String.format("Cannot get histories from DB: ", sqlException));
+      return histories;
+    }
+  }
+}

a4b8a9efd29a1e6f0c6c9cf3bf37cfa01af5d2c9
Kien Vu <kvstudio.154@gmail.com>
2021-11-06 14:24:23 +0700

    6/11/2021 2:24 PM
    Daily backup

--- src/AdminCenterController.java
+++ src/AdminCenterController.java
@@ -1,18 +1,29 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import models.History;
+import repositories.HistoryRepository;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class AdminCenterController {
+public class AdminCenterController implements Initializable {
   private Scene scene;
   private Parent root;
   @FXML
@@ -31,6 +42,120 @@ public class AdminCenterController {
       stage = (Stage) adminCenterPane.getScene().getWindow();
       stage.close();
     }
+  }
+
+  @FXML
+  private TableView<History> historyTable;
+  @FXML
+  private TableColumn<History, Integer> IdColumn;
+  @FXML
+  private TableColumn<History, String> licensePlateColumn;
+  @FXML
+  private TableColumn<History, String> vehicleTypeColumn;
+  @FXML
+  private TableColumn<History, String> seatColumn;
+  @FXML
+  private TableColumn<History, Integer> monthlyTicketColumn;
+  @FXML
+  private TableColumn<History, String> timeInColumn;
+  @FXML
+  private TableColumn<History, String> timeOutColumn;
+  @FXML
+  private TableColumn<History, String> parkingTimeColumn;
+  @FXML
+  private TableColumn<History, String> parkingFeeColumn;
+  @FXML
+  private TableColumn<History, Integer> statusColumn;
+  @FXML
+  private Button backButton, nextButton;
+  private List<History> histories = new ArrayList();
+  private HistoryRepository historyRepository = new HistoryRepository();
+
+  //internal state
+  private int pageNumber = 0;
+
+  public AdminCenterController() {
+  }
+
+  public void pressBack() {
+    pageNumber = pageNumber <= 0 ? pageNumber : pageNumber - 1;
+    pageNumberLabel.setText(String.valueOf(pageNumber + 1));
+  }
+
+  public void pressNext() {
+    pageNumber++;
+    pageNumberLabel.setText(String.valueOf(pageNumber + 1));
+  }
+
+  //select * from parking limit 25 offset 25 * 2
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    int numberOfItemPerPage = 25;
+    histories = historyRepository.getHistories(pageNumber, numberOfItemPerPage);
+    backButton.setDisable(pageNumber <= 0);
+    ObservableList<History> historyControllerObservableList = FXCollections.observableArrayList(histories);
+    IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+    licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("license_plate"));
+    vehicleTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+    seatColumn.setCellValueFactory(new PropertyValueFactory<>("seat"));
+    monthlyTicketColumn.setCellValueFactory(new PropertyValueFactory<>("ticket"));
+    timeInColumn.setCellValueFactory(new PropertyValueFactory<>("time_in"));
+    timeOutColumn.setCellValueFactory(new PropertyValueFactory<>("time_out"));
+    parkingTimeColumn.setCellValueFactory(new PropertyValueFactory<>("parking_time"));
+    parkingFeeColumn.setCellValueFactory(new PropertyValueFactory<>("fee"));
+    statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+    //Initial filtered list
+    FilteredList<History> filteredList = new FilteredList<>(historyControllerObservableList, b -> true);
+    searchBox.textProperty().addListener((observable, oldValue, newValue) -> filteredList.setPredicate(historyController -> {
+      //If no search value then display all records or whatever records it current have. No change :v
+      if (newValue.isEmpty() || newValue.isBlank()) {
+        return true;
+      }
+      String searchKeyWords = newValue.toLowerCase();
+      if (historyController.getLicense_plate().toLowerCase().contains(searchKeyWords)) {
+        return true; // Means we found a match in license_plate
+      } else if (historyController.getType().toLowerCase().contains(searchKeyWords)) {
+        return true;
+      } else if (historyController.getSeat().toLowerCase().contains(searchKeyWords)) {
+        return true;
+      } else if (historyController.getTime_in().toLowerCase().contains(searchKeyWords)) {
+        return true;
+      } else if (historyController.getTime_out().toLowerCase().contains(searchKeyWords)) {
+        return true;
+      } else if (historyController.getParking_time().toLowerCase().contains(searchKeyWords)) {
+        return true;
+      } else return historyController.getFee().toLowerCase().contains(searchKeyWords);
+    }));
+    SortedList<History> sortedList = new SortedList<>(filteredList);
+    // Bind sorted result with TableView
+    sortedList.comparatorProperty().bind(historyTable.comparatorProperty());
+    //Apply filtered and sorted data to the TableView
+    historyTable.setItems(sortedList);
+    //historyTable.setItems(historyControllerObservableList);
+  }
+
+  @FXML
+  private TextField searchBox;
+
+  @FXML
+  private Label pageNumberLabel, errorLabel, errorLabel1;
+
+  public void limitLength() {
+    searchBox.lengthProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.intValue() > oldValue.intValue()) {
+        // Check if the new character is greater than LIMIT
+        if (searchBox.getText().length() > 10) {
+          errorLabel.setText("!");
+          errorLabel1.setTextFill(Color.RED);
+          errorLabel1.setText("License Plate length must be <= 10!");
+          searchBox.setText(searchBox.getText().substring(0, 10));
+        } else {
+          errorLabel.setText("");
+          errorLabel1.setTextFill(Color.BLACK);
+          errorLabel1.setText("Admin Parking History");
+        }
+      }
+    });
   }
 
   @FXML
@@ -63,6 +188,30 @@ public class AdminCenterController {
 
   public void goToHelp() throws IOException {
     root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HelpScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAccountManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AccountManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToVehicleManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("VehicleManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToTicketManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TicketManagementScene.fxml")));
     Stage stage = (Stage) menuBar.getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);

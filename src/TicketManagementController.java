2ea0e41294a1cce2f7d65e65c49656733cdab502
Kien Vu <kvstudio.154@gmail.com>
2021-11-10 17:28:26 +0700

    Create Ticket Management Controller

--- src/TicketManagementController.java
+++ src/TicketManagementController.java
@@ -1,12 +1,26 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import models.Ticket;
+import repositories.TicketRepository;
 
-public class TicketManagementController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class TicketManagementController implements Initializable {
   private Scene scene;
   private Parent root;
   @FXML
@@ -38,12 +52,108 @@ public class TicketManagementController {
   @FXML
   private MenuBar menuBar;
   @FXML
-  private TableView<?> ticketTable;
+  private TableView<Ticket> ticketTable;
   @FXML
-  private TableColumn<?, ?> expiredDateColumn;
+  private TableColumn<Ticket, String> expiredDateColumn;
   @FXML
-  private TableColumn<?, ?> licensePlateColumn;
+  private TableColumn<Ticket, String> licensePlateColumn;
   @FXML
-  private TableColumn<?, ?> statusColumn;
+  private TableColumn<Ticket, String> statusColumn;
 
+  private List<Ticket> tickets = new ArrayList();
+  private TicketRepository ticketRepository = new TicketRepository();
+
+  public TicketManagementController() {
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    tickets = ticketRepository.getTicket();
+    ObservableList<Ticket> ticketObservableList = FXCollections.observableArrayList(tickets);
+    licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("license_plate"));
+    expiredDateColumn.setCellValueFactory(new PropertyValueFactory<>("expired_date"));
+    statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+    ticketTable.setItems(ticketObservableList);
+  }
+
+  public void goToIn() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToOut() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OutScene.fxml")));
+    //stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToHelp() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HelpScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAbout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AboutScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void logout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToHistory() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HistoryScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAdminPackingHistory() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AdminCenterScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void adminLogout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AdminScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToVehicleManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("VehicleManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAccountManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AccountManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 }

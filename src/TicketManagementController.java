f4d7ebf03e1e54a9191681a45ac3b05811154acc
Kien Vu <kvstudio.154@gmail.com>
2021-11-11 10:56:51 +0700

    Create add monthly ticket function

--- src/TicketManagementController.java
+++ src/TicketManagementController.java
@@ -9,16 +9,26 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import models.Ticket;
+import repositories.Database;
 import repositories.TicketRepository;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TicketManagementController implements Initializable {
   private Scene scene;
@@ -68,12 +78,99 @@ public class TicketManagementController implements Initializable {
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    resetTable();
+  }
+
+  public void resetTable() {
     tickets = ticketRepository.getTicket();
     ObservableList<Ticket> ticketObservableList = FXCollections.observableArrayList(tickets);
     licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("license_plate"));
     expiredDateColumn.setCellValueFactory(new PropertyValueFactory<>("expired_date"));
     statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
     ticketTable.setItems(ticketObservableList);
+  }
+
+  public void addLicenseLimitLength() {
+    addLicensePlateTextField.lengthProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.intValue() > oldValue.intValue()) {
+        // Check if the new character is greater than LIMIT
+        if (addLicensePlateTextField.getText().length() > 10) {
+          error1.setTextFill(Color.RED);
+          error.setTextFill(Color.RED);
+          error.setText("License Plate length must be <= 10!");
+          error1.setText("!");
+          // if its 11th character then just setText to previous one
+          addLicensePlateTextField.setText(addLicensePlateTextField.getText().substring(0, 10));
+        } else {
+          error.setText("");
+          error1.setText("");
+        }
+      }
+    });
+  }
+
+  public void addCheck() {
+    if (addLicensePlateTextField.getText().isEmpty()) {
+      error1.setTextFill(Color.RED);
+      error.setTextFill(Color.RED);
+      error.setText("Please enter license plate!");
+      error1.setText("!");
+    } else {
+      Connection connection = null;
+      PreparedStatement preparedStatement = null;
+      ResultSet resultSet = null;
+      try {
+        connection = Database.getInstance().getConnection();
+        preparedStatement = connection.prepareStatement("select * from ticket where license_plate = ?");
+        preparedStatement.setString(1, addLicensePlateTextField.getText());
+        resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+          error1.setTextFill(Color.RED);
+          error.setTextFill(Color.RED);
+          error.setText("Ticket existing!");
+          error1.setText("!");
+        } else {
+          preparedStatement = connection.prepareStatement("insert into ticket(license_plate, expired_date) values(?, ?)");
+          preparedStatement.setString(1, addLicensePlateTextField.getText());
+          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+          LocalDateTime now = LocalDateTime.now();
+          if (add1Month.isSelected()) {
+            now = now.plusMonths(1);
+          } else if (add6Months.isSelected()) {
+            now = now.plusMonths(6);
+          } else if (add1Year.isSelected()) {
+            now = now.plusYears(1);
+          }
+          preparedStatement.setString(2, dtf.format(now));
+          int kq = preparedStatement.executeUpdate();
+          error1.setText("");
+          if (kq > 0) {
+            error.setTextFill(Color.GREEN);
+            error.setText("Added " + addLicensePlateTextField.getText() + "!");
+            resetTable();
+          } else {
+            error.setTextFill(Color.RED);
+            error.setText("We can't add your ticket at this time. Please try again!");
+          }
+        }
+      } catch (SQLException e) {
+        Logger.getLogger(TicketManagementController.class.getName()).log(Level.SEVERE, null, e);
+      } finally {
+        try {
+          if (resultSet != null) {
+            resultSet.close();
+          }
+          if (preparedStatement != null) {
+            preparedStatement.close();
+          }
+          if (connection != null) {
+            connection.close();
+          }
+        } catch (SQLException e) {
+          Logger.getLogger(TicketManagementController.class.getName()).log(Level.SEVERE, null, e);
+        }
+      }
+    }
   }
 
   public void goToIn() throws IOException {

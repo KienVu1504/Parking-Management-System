160d684e2a2d48b0723629b240ae1e91e8506c93
Kien Vu <kvstudio.154@gmail.com>
2021-11-21 16:09:11 +0700

    Update Price Management Scene and Controller

--- src/PriceManagementController.java
+++ src/PriceManagementController.java
@@ -1,2 +1,44 @@
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import repositories.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PriceManagementController {
+  private Scene scene;
+  private Parent root;
+  @FXML
+  private AnchorPane pricePane;
+  @FXML
+  private TextField bicyclesField, motorbikeField, seat1, seat2, seat3, upBicycles, upMotorbike, upSeat1, upSeat2, upSeat3;
+  @FXML
+  private Label error;
+  @FXML
+  private MenuBar menuBar;
+
+  public void closeAPP() {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Close!");
+    alert.setHeaderText("You're about to close the application!");
+    alert.setContentText("Do you want to exit?");
+    // Get the Stage.
+    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+    // Add a custom icon.
+    stage.getIcons().add(new Image("images/sgd.png"));
+    if (alert.showAndWait().orElse(null) == ButtonType.OK) {
+      stage = (Stage) pricePane.getScene().getWindow();
+      stage.close();
+    }
+  }
 }

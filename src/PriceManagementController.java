a9b6415abde3f17f889642a7e0acd8c9d522fe28
Kien Vu <kvstudio.154@gmail.com>
2021-11-22 10:58:06 +0700

    Create Price management controller

--- src/PriceManagementController.java
+++ src/PriceManagementController.java
@@ -1,2 +1,130 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class PriceManagementController {
+  private Scene scene;
+  private Parent root;
+  @FXML
+  private AnchorPane pricePane;
+  @FXML
+  private TextField upBicycles1, upBicycles2, upBicycles3, upMotorbike1, upMotorbike2, upMotorbike3, up1seat1, up1seat2, up1seat3, up2seat1, up2seat2, up2seat3, up3seat1, up3seat2, up3seat3;
+  @FXML
+  private Label error, currentBicycles1, currentBicycles2, currentBicycles3, currentMotorbike1, currentMotorbike2, currentMotorbike3, current1seat1, current1seat2, current1seat3, current2seat1, current2seat2, current2seat3, current3seat1, current3seat2, current3seat3;
+  @FXML
+  private MenuBar menuBar;
+
+  public void closeAPP() {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Close!");
+    alert.setHeaderText("You're about to close the application!");
+    alert.setContentText("Do you want to exit?");
+    // Get the Stage.
+    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+    // Add a custom icon.
+    stage.getIcons().add(new Image("images/sgd.png"));
+    if (alert.showAndWait().orElse(null) == ButtonType.OK) {
+      stage = (Stage) pricePane.getScene().getWindow();
+      stage.close();
+    }
+  }
+
+  
+
+  public void goToAccountManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AccountManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToIn() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToOut() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OutScene.fxml")));
+    //stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToHelp() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HelpScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAbout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AboutScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void logout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToHistory() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HistoryScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToAdminPackingHistory() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AdminCenterScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void adminLogout() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AdminScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToVehicleManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("VehicleManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void goToTicketManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TicketManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 }

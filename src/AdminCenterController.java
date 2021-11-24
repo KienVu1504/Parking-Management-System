370d01a7c1a207ec0235b76dadeab45ce5a8ede9
Kien Vu <kvstudio.154@gmail.com>
2021-11-24 15:42:13 +0700

    Fix KeyCodeCombination

--- src/AdminCenterController.java
+++ src/AdminCenterController.java
@@ -219,6 +219,20 @@ public class AdminCenterController implements Initializable {
     stage.setScene(scene);
     stage.show();
   }
+  public void goToSlotsManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SlotsManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+  public void goToPricesManagement() throws IOException {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("PricesManagementScene.fxml")));
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 
   public void goToAbout() throws IOException {
     root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AboutScene.fxml")));
